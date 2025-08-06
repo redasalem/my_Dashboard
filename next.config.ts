2b023953d41f038548e30231adbb8a90a666c7dd
Reda Salem <redasalem287@gmail.com>
2025-08-06 23:47:18 +0300

    custmize Eslint

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images:{
-    remotePatterns:[
+  images: {
+    remotePatterns: [
       {
-        protocol:"https",
-        hostname:"images.pexels.com",
-      }
-    ]
-  }
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
+
